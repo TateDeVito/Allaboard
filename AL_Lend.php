587c46af37b406af8d01f6154bfc303f78d1dde9
Tate DeVito <tcd2tq@virginia.edu>
2019-08-19 13:20:39 -0400

    Update AL_Lend.php

--- AL_Lend.php
+++ AL_Lend.php
@@ -4,9 +4,7 @@
 <head>
 
 <?php 
-
 	include 'AL_Head.php';
-
 ?>
 
 </head>
@@ -14,16 +12,16 @@
 
 	<center>
 	<p><b>Fill out item info below to add equipment:</b><br>
-	<form name="list" onsubmit="return validateForm() ;" method="post">
+	<form action="LendHandler.php" method="post">
 
 	<!-- Cannot be empty -->
 	<!-- Drop down should be a list of companies, and then an Other: -->
 	Brand <input type="Text" name="brand"><br> 
 
 	<!-- Cannot be empty -->
-	Item name <input type="Text" name="name"><br>
+	Item name <input type="Text" name="item"><br>
 	Rent price <input type=number name="rentPrice"><br><br>
-	<input type="submit" value="List" name="list" onclick="validate()"><br>
+	<input type="submit" value="List" name="list"><br>
 	</form>	
 	</p>
 	</center>
