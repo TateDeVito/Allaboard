cbf8a4b710c36a2760097caf912c2e6c8a2cd394
Tate DeVito <tcd2tq@virginia.edu>
2019-08-19 13:00:34 -0400

    Update AL_Lend.php

--- AL_Lend.php
+++ AL_Lend.php
@@ -15,7 +15,12 @@
 	<center>
 	<p><b>Fill out item info below to add equipment:</b><br>
 	<form name="list" onsubmit="return validateForm() ;" method="post">
+
+	<!-- Cannot be empty -->
+	<!-- Drop down should be a list of companies, and then an Other: -->
 	Brand <input type="Text" name="brand"><br> 
+
+	<!-- Cannot be empty -->
 	Item name <input type="Text" name="name"><br>
 	Rent price <input type=number name="rentPrice"><br><br>
 	<input type="submit" value="List" name="list" onclick="validate()"><br>
