18fc07a4b83c085b257e24503e100a028e2b539e
Tate DeVito <tcd2tq@virginia.edu>
2019-08-19 13:20:35 -0400

    Create LendHandler.php

---
+++ LendHandler.php
@@ -0,0 +1,13 @@
+<?php
+	if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        echo 'good';
+		// if(!empty($_POST['brand'] && !empty($_POST['item'])){
+		// 	$brand = $_POST['brand']
+		// 	$item = $_POST['item']
+		// }
+
+		// else {
+						
+		// }
+	}
+?>
