a00af7ab80f94832a2af6cdb11940d8bc328c1b0
Tate DeVito <tcd2tq@virginia.edu>
2019-08-20 18:27:28 -0400

    Db insertions

--- Db/connectToDb.php
+++ Db/connectToDb.php
@@ -2,23 +2,25 @@
 <!-- submit the equipment to the larger pool of available equipment -->
 
 <?php
-    $host = '127.0.0.1';
+    $host = 'localhost';
+    $port = 3306;
     $dbname = 'equipmentpool';
     $username = 'root';
     $pass = '';
 
-    $dsn = "mysql:hostname=$host;dbname=$dbname";
+    $dsn = "mysql:hostname=$host;port=$port;dbname=$dbname";
 
     try {
         // PHP Data Objects (PDO's) require a dsn, username, and password
         $db = new PDO($dsn, $username, $pass);
+        // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo 'Connected to database!';
     }
 
     catch(PDOException $ex) {
         // Shouts the error message if there is one connecting to the database
         $error = $ex->getMessage();
-        echo 'Connection failure: ' . $error;
-        die("Connection terminated");
+        echo "Error: " . $error . "<br>";
+        die("...Connection terminated");
     }
 ?>
--- Db/insertToDb.php
+++ Db/insertToDb.php
@@ -14,7 +14,7 @@
 			$item = $_POST['item'];
 			// price (varchar 30)
 			// the reason for a varchar is for a name your price element
-			$price = "???";
+			$price = 0;
 			if(!empty($_POST['rent'])) { $price = $_POST['rent']; }
 			// photo
 			$photo = "";
@@ -22,27 +22,27 @@
 		echo "<br><br>Adding a <b>" . $brand . " " . $item; 
 		echo "; Price: $" . $price;
 
-		// URLs': 1.) https://www.w3schools.com/sql/sql_insert.asp
+		// URLs': 
+		// 1.) https://www.w3schools.com/sql/sql_insert.asp
 		// 2.) https://www.w3schools.com/php/php_mysql_insert.asp
 
 		// "INSERT INTO `DBName` (colq, col2 ...) VALUES ('A', 'B' ...);"
 		// If you are using all columns, then you do not need to specify which cols to 
 		// pull from.
-		$insertItem = "INSERT INTO $dbname
+
+		$insertItem = "INSERT INTO `$dbname` 
 		VALUES($userID, $location, $brand, $item, $price, $photo)";
 
 		echo "<br><br>"; 
 
 		// To run the queries on a PDO, we use the exec() method
 		try {
-			$db->exec($insertItem);
+			$db->query($insertItem);
 			echo 'Successful entry!';
 		}
 
 		catch(PDOException $e) { echo 'Error: ' . $e->getMessage(); }
 
 		$db = null;
-
-		// Insertions still not going through - look into if the server is correct
 	}
 ?>
