e65a3a80e08830d3e2e2b79704390e0d81e18ee5
Tate DeVito <tcd2tq@virginia.edu>
2019-08-20 12:32:27 -0400

    Update Rider.js

--- Rider.js
+++ Rider.js
@@ -1,31 +1,7 @@
-
-public class Rider {
-	String user; //Unique, primary identifier
-	String first;
-	String last;
-	String email;
-	//password
-	//AND - location city;
-	
-	function Rider(String f, String l) { // constructor
-		this.first = f;
-		this.last = l;
-		//user - must be unique - we generate
-	}
-	
-	function getName() {
-		return first + " " + last;
-		}
-	
-	//public String genUsername() {
-	//}
-
-	
-//tests on the rider class	
-//public static void main(String[] args) {
-//	Rider Tate = new Rider("Tate", "DeVito");{ // note object setup
-//	}		
-//	System.out.println(Tate.getName());		
-//}
-
-}
+var Rider;
+// 	string user; //Unique, primary identifier
+// 	String first;
+// 	String last;
+// 	String email;
+// 	//password
+// 	//AND - location city;
