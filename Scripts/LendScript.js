5c882acf96810ed819aca7aba87ab8104ee50816
Tate DeVito <tcd2tq@virginia.edu>
2019-08-20 12:22:50 -0400

    Update LendScript.js

--- Scripts/LendScript.js
+++ Scripts/LendScript.js
@@ -2,14 +2,19 @@
 var brand = document.getElementById('brand');
 var item = document.getElementById('item');
 var error = document.getElementById('error');
+var btn = document.getElementById('list'); 
 
 // Must check that neither the brand and item are empty
 function itemCheck() {
     if(brand.value == "" || item.value == "") {
-        error.innerHTML = "Form missing element";
+
         if(brand.value == "")  { brand.style = 'border-color : red' }
         if(item.value == "")  { item.style = 'border-color : red' }
-        
+
+        error.innerHTML = "Form missing element(s)";
+        brand.innerHTML = brand.value;
+        brand.innerHTML = brand.value;
+
         return false;
     }
 }
