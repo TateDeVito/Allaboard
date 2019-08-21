4aec4986bd9cfefa66f7651644ca7293725913fa
Tate DeVito <tcd2tq@virginia.edu>
2019-08-21 09:30:02 -0400

    Tweaks

--- Scripts/LendScript.js
+++ Scripts/LendScript.js
@@ -3,17 +3,23 @@ var brand = document.getElementById('brand');
 var item = document.getElementById('item');
 var error = document.getElementById('error');
 
-// Must check that neither the brand and item are empty
+// Form conditions
+// A. Neither the brand and item are empty
+// B. No apostraphe's included in the listing
 function itemCheck() {
     if(brand.value == "" || item.value == "") {
 
         if(brand.value == "")  { brand.style = 'border-color : red' }
         if(item.value == "")  { item.style = 'border-color : red' }
-
         error.textContent = "Form missing element(s)";
-        brand.innerHTML = brand.value;
-        brand.innerHTML = brand.value;
 
-        return false;
-    }
+        return false; }
+
+    if(brand.value.includes("m")){
+
+        error.textContent = "m";
+
+        return false; }
+
+    else { return true; }
 }   
