489bdc591b006f5ad7cd6615b2df5ae218d29e3d
Tate DeVito <tcd2tq@virginia.edu>
2019-08-23 00:45:49 -0400

    About page

--- AL_About.php
+++ AL_About.php
@@ -8,13 +8,26 @@
 ?>
 </header>
 
-<title>About</title>
+<title>Hi!</title>
 
 <body>
 	<center>
+	<p class="bio">
+	Hi!
+	<br><br>
+	My name is <b> Tate DeVito</b>, and I am the founder of <b>allaboard</b>
 	<br>
-	About page
-	<br>
+	Growing up, I have always loved snowboarding and surfing. All along the way, 
+	the thing that has stood in my way is the costly process of buying or renting equipment. 
+	<br><br>
+	Renting equipment was no fun and the equipment was never high quality; buying equipment was super expensive. 
+	I thought it might be interesting if I built a platform to help both parties. 
+	<br><br>
+	With <b> allaboard</b>, people who have paid a lot of money for equipment can make some money back by 
+	letting others use it in the off season. The renting pool of people can test and use high 
+	quality equipment from the first group! Win win. 
+	<br><br>
+	A board for everyone (all a board). All aboard. 
 	</center>
 </body>
 </html>
