ca8e2666f9ea2721328fb0de75ce2224ab4ccac5
Tate DeVito <tcd2tq@virginia.edu>
2019-08-27 17:19:11 -0400

    signin tweaks

--- signUp.php
+++ signUp.php
@@ -15,12 +15,31 @@
 <form class="forms" action="welcomeUser.php" onclick="return signInCheck()" method="POST">
 First Name: <input class="twenty" type="textbox" id="fname" name="fname">
 <!-- Must not be empty -->
+
 Last Name: <input class="twenty" type="textbox" id="lname" name="lname"><br>
 <!-- Must not be empty -->
+
 Username: <input class="twenty" type="textbox" id="user" name="user"><br>
-<div style="color: red; font-weight: bold" id="userErr"></div>
+<div style="color: red; font-weight: bold" id="userErr">
+
+<!-- open php
+	// Must confirm that username is not in the database
+	if($_SERVER['REQUEST_METHOD'] == 'POST') {
+		require('connectToDb');
+		$user = $_POST['user'];
+
+		// Username must not be in use already
+		$exist = "SELECT COUNT(userID) FROM users WHERE $user == 'userID"; 
+		$count = $db->exec($exist);
+
+		if($exist > 0) { echo 'username taken'; }
+	}
+close php -->
+
+</div>
 <!-- Must not be empty -->
 <!-- Must not be an existing username -->
+
 Password: <input class="twenty" type="textbox" id="pass" name="pass">
 <div style="color: red; font-weight: bold" id="passErr"></div>
 <!-- Must be 8-24 characters -->
